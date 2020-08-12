1e018a8d34c85d9f82edaef577ead0cfd75f1b72
Jason <jxfireruby@users.noreply.github.com>
2020-08-12 18:59:32 -0400

    add client side js file

---
+++ public_html/playerView.js
@@ -0,0 +1,10 @@
+//Enter Username and select class input
+    //Default route is /
+    //Send POST to / to add username + class
+    //Return Default Stats and attacks ID in database and currernt round timer
+    //
+
+//Each attack sends another POST request to /attack
+//Attack ID, and target ID
+
+
