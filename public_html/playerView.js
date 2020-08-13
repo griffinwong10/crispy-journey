2e641cbcb69c3ed4112f3eac5e1f598db650e91c
Jason <jxfireruby@users.noreply.github.com>
2020-08-13 09:58:08 -0400

    Enter Username Screen works in theory

--- public_html/playerView.js
+++ public_html/playerView.js
@@ -1,3 +1,33 @@
+"use strict";
+document.addEventListener('DOMContentLoaded', function(event){
+    let classSelectDiv = document.getElementById("class-select");
+    //fetch classes from db and populate classSelectDiv
+
+    let submitNameBtn = document.getElementById("submit-name-btn");
+    submitNameBtn.addEventListener("click", function(){
+        let name = document.getElementById("name-input").value;
+        //let class TODO after pulls from db
+        console.log(name);
+        fetch('/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({"new-user": name, "class": "cleric"}), //TODO class change to variable
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+            let overlay = document.getElementById("overlay");
+            overlay.style.display = "none";
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+    });
+});
+
+
 //Enter Username and select class input
     //Default route is /
     //Send POST to / to add username + class
