f53b198346df4db6e1b00687033d24a1f2e56d1b
Griffin Wong <griffinwong@Griffins-MacBook-Air.local>
2020-08-13 21:24:50 -0700

    Added code for getCharactersFromDatabase function

--- app/game.js
+++ app/game.js
@@ -1,3 +1,32 @@
+// AUTHOR: Johnathan Eberly, Griffin Wong
+// DATE: 08/12/2020
+// COURSE: CS 375-004
+// TEAM: Crispy Journey
+// PURPOSE: Database/Backend Functions
+
+// Express constants
+// Griffin Wong 08/12/2020
+const pg = require("pg");
+const express = require("express");
+const app = express();
+const port = 3000;
+const hostname = "localhost";
+
+// Database connections
+// Griffin Wong 08/12/2020
+const Pool = pg.Pool;
+const pool = new Pool(env);
+
+pool.connect().then(function () {
+    console.log(`Connected to database ${env.database}`);
+});
+
+// Middleware
+// Griffin Wong 08/12/2020
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+
+
 function queryDatabaseForClient(client, fields){
 	// TODO: Query database for the listed fields of the given client ID
 	// Return as a dict
@@ -16,10 +45,62 @@ function clientAsksForStats(client){
 	sendStatsToClient(client, stats)
 }
 
+
+
+// Griffin Wong 08/12/2020
+// Query database for current players
+// Returns a a list of IDs
 function getCharactersFromDatabase(){
-	// TODO: Query database for current players
-	// Return as a list of IDs
+
+// // Select all players that are in a specific room
+// // TODO: Determine if queryStringTwo is necessary
+// let queryStringTwo = 'SELECT players.player_id FROM players WHERE players.room_id = $1';
+// let valueTwo = [replaceThisValue];
+
+// Select all players that are still in the match
+let queryStringOne = 'SELECT players.player_id FROM players WHERE players.is_dead = FALSE';
+
+pool.query(queryStringOne, (err, result) => {
+	if (err) {
+	return console.error('Error executing query', err.stack)
+	console.log(err)
+	} else {
+
+		// Initialize array
+		let activePlayerList = [];
+
+		// Create list of all player IDs
+		for (let i=0; i < result.rows.length; i++) {
+			activePlayerList.push(result.rows[i]);
+		}
+
+		// Create JS Object
+		let activePlayersObject = {
+			players: activePlayerList,
+		};
+
+		// Send Response to Client
+		res.setHeader("Content-Type", "application/json");
+		res.status(200).send(jsonResonseBodyTwo); // JSON Body
+		console.log(jsonResonseBodyTwo);
+	}
+})
 }
+
+
+// // Requirement 1F; Parameterized Query Insertion
+// // Reference: https://node-postgres.com/features/queries#parameterized-query
+// let text = 'INSERT INTO books(title, genre, quality) VALUES($1, $2, $3)';
+// let values = [req.body.title, req.body.genre, req.body.quality];
+
+// // Requirement 1E; Insert Book
+// pool.query(text, values, (err, res) => {
+//     if (err) {
+//         console.log("Error", err.stack);
+//     } else {
+//         console.log("Book has been added!");
+//     }
+// });
 
 function clientAsksForTargets(client){
 	let possibleTargets = getCharactersFromDatabase
