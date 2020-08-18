2d4c7f669b425aa285cdd4b0d7fe1f2f203ac2d9
Jason <jxfireruby@users.noreply.github.com>
2020-08-18 10:29:24 -0400

    Overlay UI changed

--- app/public_html/playerView.js
+++ app/public_html/playerView.js
@@ -1,21 +1,26 @@
 "use strict";
 document.addEventListener('DOMContentLoaded', function(event){
     let classSelectDiv = document.getElementById("class-select");
+    //fetch from /
     //For loop through db json return
     let aClass = document.createElement("option");
     aClass.value = "warrior";
     aClass.textContent = "warrior";
     classSelectDiv.appendChild(aClass);
     let aClass2= document.createElement("option");
-    aClass2.value = "sheild";
-    aClass2.textContent = "sheild";
+    aClass2.value = "shield";
+    aClass2.textContent = "sheld";
     classSelectDiv.appendChild(aClass2);
 
     let submitNameBtn = document.getElementById("submit-btn");
     submitNameBtn.addEventListener("click", function(){
         let name = document.getElementById("username-input").value;
-        //let class TODO after pulls from db
+        let userClass = document.getElementById("class-select").value;
+        
+        /*Test Log */
         console.log(name);
+        console.log(userClass);
+
         if(name.length === 0){
             let usernameError = document.getElementById("username-error");
             usernameError.style.display = "block";
@@ -34,6 +39,7 @@ document.addEventListener('DOMContentLoaded', function(event){
                 console.log('Success:', data);
                 let overlay = document.getElementById("overlay");
                 overlay.style.display = "none";
+                //stats for player here
             })
             .catch((error) => {
                 console.error('Error:', error);
@@ -49,13 +55,11 @@ document.addEventListener('DOMContentLoaded', function(event){
     //Default route is /
     //Send POST to / to add username + class
     //Return Default Stats and attacks ID in database and currernt round timer
-    //
 
 //Each attack sends another POST request to /attack
 //Attack ID, and target ID
 
 //TODO
-//Test Class gen on front page
 //Test attack gen from db
 //Test cd and sending fetch
 
