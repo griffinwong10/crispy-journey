14db296811d5958ae84997298ae36341d5f3e679
Griffin Wong <griffinwong@Griffins-MacBook-Air.local>
2020-08-17 20:55:51 -0700

    Removed duplicate imports and constants

--- app/server.js
+++ app/server.js
@@ -1,37 +1,31 @@
 // AUTHORS: Johnathan Eberly, Griffin Wong, Jason Liu, Ryan Kalbach
-// DATE: 08/13/2020
+// DATE: 08/17/2020
 // COURSE: CS 375-004
 // TEAM: Crispy Journey
-// PURPOSE: Server Side Code
+// PURPOSE: Server-Side Code
 
-// TODO IMPORT FUNCTIONS FROM game.js TO USE IN ROUTE HANDLERS GW 08/13/2020
+// TODO: 
+// 1. IMPORT DB FUNCTIONS FROM game.js TO USE IN ROUTE HANDLERS GW 08/13/2020
+// 2. 
+// 3. 
+// 4. 
+// 5.
 
 
 const express = require("express");
 const app = express();
-
 const port = 3000;
 const hostname = "localhost";
-
-// Express constants
-// Griffin Wong 08/12/2020
 const pg = require("pg");
-const express = require("express");
-const app = express();
-const port = 3000;
-const hostname = "localhost";
-
-// Database connections
-// Griffin Wong 08/12/2020
 const Pool = pg.Pool;
 const pool = new Pool(env);
 
+// Create Database Connection
 pool.connect().then(function () {
     console.log(`Connected to database ${env.database}`);
 });
 
-// Middleware
-// Griffin Wong 08/12/2020
+// Use Middleware for parsing JSON
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 
