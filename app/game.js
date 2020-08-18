7960d5c9a27805de8ca2f3f73a787aa603bd772c
Griffin Wong <griffinwong@Griffins-MacBook-Air.local>
2020-08-17 20:55:27 -0700

    Fixed some incorrect queries in database functions

--- app/game.js
+++ app/game.js
@@ -1,52 +1,68 @@
-// AUTHOR: Johnathan Eberly, Griffin Wong
-// DATE: 08/12/2020
+// AUTHORS: Johnathan Eberly, Griffin Wong, Jason Liu, Ryan Kalbach
+// DATE: 08/17/2020
 // COURSE: CS 375-004
 // TEAM: Crispy Journey
-// PURPOSE: Database/Backend Functions
+// PURPOSE: Database and Backend Functions
 
-// Express constants
+
+// TODO:
+// 1. Determine what fields of the given client ID need to be returned for 
+// 			queryDatabaseForClient function Griffin Wong 08/17/2020
+// 2. 
+// 3. 
+// 4. 
+// 5. 
+
+
+
 // Griffin Wong 08/12/2020
 const pg = require("pg");
 const express = require("express");
 const app = express();
 const port = 3000;
 const hostname = "localhost";
-
-// Database connections
-// Griffin Wong 08/12/2020
 const Pool = pg.Pool;
 const pool = new Pool(env);
 
+// Create Database Connection
 pool.connect().then(function () {
     console.log(`Connected to database ${env.database}`);
 });
 
-// Middleware
-// Griffin Wong 08/12/2020
+// Use Middleware for parsing JSON
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 
 
+
+// TODO: Query database for the listed 
+// fields of the given client ID and Return 
+// as a dict Johnathan Eberly ND/ND/2020
 function queryDatabaseForClient(client, fields){
-	// TODO: Query database for the listed fields of the given client ID
-	// Return as a dict
+
 }
 
+
 // Griffin Wong 08/13/2020
-// Parameterized Query Insertion
 // Reference: https://node-postgres.com/features/queries#parameterized-query
 
-// TODO This may need to be placed in the server.js file to allow for the
+// TODO: This may need to be placed in the server.js file to allow for the
 // 	request body to be parsed using middleware and to extract/submit
 // 	information to the database
 
+// NOTE: These were the only two form fields
+// that I saw on playerView.html,
+// we can discuss if there are more params
+// Griffin Wong 08/17/2020
+
 // Create Player with Username and Class Type
+// that uses a parameterized Query Insertion
 function sendToDatabaseForClient(client, data){
 
-	let createPlayerOne = 'INSERT INTO player(username, fk_class) VALUES($1, $2)';
-	let createPlayerValuesOne = [req.body.username, req.body.class];
+	let createPlayer = 'INSERT INTO player(username, class_id) VALUES($1, $2)';
+	let createPlayerValues = [req.body.username, req.body.class];
 
-	pool.query(createPlayerOne, createPlayerValuesOne, (err, res) => {
+	pool.query(createPlayer, createPlayerValues, (err, res) => {
 	    if (err) {
 	        console.log("Error", err.stack);
 	    } else {
@@ -55,40 +71,58 @@ function sendToDatabaseForClient(client, data){
 	});
 }
 
+
+
 // Griffin Wong 08/13/2020
 // Parameterized Query Insertion
 // Reference: https://node-postgres.com/features/queries#parameterized-query
 
-// NOTE: There may be an issue with selecting attack_strength due to the join table
-// TODO: Send HP, Attack Strength (ATK), Armor to client.
+// NOTE: The query to select attack_strength may be incorrect
+// TODO: Send HP, Attack Strength (ATK), Armor to client
 function sendStatsToClient(client, stats){
 	
-	let getPlayerInfoString = 'SELECT health, attack_strength, armor FROM player WHERE player.player_id = $1';
+	let getPlayerInfoString = 'SELECT health, armor FROM player WHERE player_id = $1';
 	let getPlayerInfoValues = [req.query.player_id];
+	let getPlayerAttackStrength = 'SELECT attack_strength FROM attack WHERE attack.attack_player_id = player.attack_player_id';
 
+	let playerInfoArr = [];
+
+	// Get health and armor from player table
 	pool.query(getPlayerInfoString, getPlayerInfoValues, (err, result) => {
 		if (err) {
 		  return console.error('Error executing query', err.stack)
 		  console.log(err)
 		} else {
-
-			let playerInfoArr = [];
-
 			for (let i=0; i < result.rows.length; i++) {
 				playerInfoArr.push(result.rows[i]);
 			}
-
 			// Create JS Object
 			let jsonResonseBody = {
 				rows: playerInfoArr,
 			};
-
-			// Send Response to Client
-			res.setHeader("Content-Type", "application/json");
-			res.status(200).send(jsonResonseBody);
-			console.log(jsonResonseBody);
 		}
-	})
+	});
+
+	// Get attack strength from attack table
+	pool.query(getPlayerAttackStrength, (err, result) => {
+		if (err) {
+		  return console.error('Error executing query', err.stack)
+		  console.log(err)
+		} else {
+			for (let i=0; i < result.rows.length; i++) {
+				playerInfoArr.push(result.rows[i]);
+			}
+			// Create JS Object
+			let jsonResonseBody = {
+				rows: playerInfoArr,
+			};
+		}
+	});
+
+	// Send Response to Client
+	res.setHeader("Content-Type", "application/json");
+	res.status(200).send(jsonResonseBody);
+	console.log(jsonResonseBody);
 }
 
 
@@ -99,13 +133,15 @@ function clientAsksForStats(client){
 }
 
 
-// Griffin Wong 08/12/2020
+// Griffin Wong 08/17/2020
 // Query database for current players
 // Returns a a list of IDs
 function getCharactersFromDatabase(){
 
+	let roomValue; // Define this for each player
+
 	// Select all players that are in a specific room
-	let queryStringOne = 'SELECT player.player_id FROM player WHERE player.room_id = $1';
+	let queryStringOne = 'SELECT player_id FROM player WHERE room_id = $1';
 
 	// Change variable of roomValue with whatever room is being queried
 	let valueTwo = [roomValue];
